e44542f4861ef96105e4a084b873134b04403ac5
helio <helio@c489cccc-4d05-0410-97be-cfb9d72d6f98>
2009-12-16 15:17:41 +0000

    * Simetrização parcial completa.
    git-svn-id: http://home.speed.dcc.ufmg.br:8080/svn/public/helio/scan@5079 c489cccc-4d05-0410-97be-cfb9d72d6f98

--- proximity.cpp
+++ proximity.cpp
@@ -36,6 +36,40 @@ Proximity::~Proximity() {
 double Proximity::getProximity(uint i, uint j) {
 }
 
+void Proximity::partialSymmetrization(float beta) {
+     // First, let's mark all virtual edges that should be created
+     // with a 9999.0
+     for (int i = 0; i < n; ++i) {
+          for (int j = 0; j < n; ++j) {
+               if (P[i][j] > 0.0 && P[j][i] == 0.0) {
+                    P[j][i] = 9999.0;
+               }
+          }
+     }
+     // Now we will fix the transition probabilities considering that
+     // the virtual edges have beta probability
+     for (int i = 0; i < n; ++i) {
+          float prob_edges = 1.0;
+          std::set<uint> edges;
+          for (int j = 0; j < n; ++j) {
+               if (P[i][j] > 0.0) {
+                    if (P[i][j] == 9999.0) {
+                         P[i][j] = beta;
+                         prob_edges -= beta;
+                    } else {
+                         edges.insert(j);
+                    }
+               }
+          }
+          // Put the right transition values on the edges
+          std::set<uint>::iterator setIt;
+          for (setIt = edges.begin(); setIt != edges.end(); ++setIt) {
+               P[i][*setIt] = prob_edges/(float)edges.size();
+          }
+     }
+
+}
+
 void Proximity::printTransitionMatrix() {
      for (int i = 0; i < n; ++i) {
           for (int j = 0; j < n; ++j) {
--- proximity.hpp
+++ proximity.hpp
@@ -5,6 +5,8 @@ class Proximity {
           SquareMatrix<float> P;
           uint n;    // # nodes
           Proximity(); // Construtor default desabilitado
+          // Creates virtual symmetric edges with beta probability
+          void partialSymmetrization(float beta);
           // Construtor de cópia de atribuição?
      public:
           Proximity(Graph& g);
